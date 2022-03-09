1ed73c27fb8bae182d6b7a06f4df355c1ab5a763
Owen Dearman <owendearman@gmail.com>
2022-03-09 09:58:53 +0000

    fixed useState

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,61 +1,53 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function EmojiPicker(): JSX.Element {
-  const [emojiFromCurrentRender, queuedEmojiForRender] = useState("");
-  const [storedEmojis, queuedEmojiStore] = useState<string[]>([]);
+  const [state, setState] = useState<string[]>([""]);
 
-
-  const setStorage = () => {
-    queuedEmojiStore(prev => [...prev, emojiFromCurrentRender]);
-  }
+  const setStorage = (string: string) => {
+    setState(() => [string, ...state]);
+  };
 
   const clearStorage = () => {
-    queuedEmojiStore([])
-  }
+    setState([]);
+  };
 
-  let storeOfEmojis: JSX.Element[] = storedEmojis.map(emojiStorageList)
-  if(storeOfEmojis.length > 5){
-    storeOfEmojis = storeOfEmojis.splice(-5)
+  let storeOfEmojis: JSX.Element[] = state.map(emojiStorageList);
+  if (storeOfEmojis.length > 5) {
+    storeOfEmojis = storeOfEmojis.splice(0, 5);
   }
 
   return (
     <>
-    <h1>Emoji picker</h1>
-    <ul style={{listStyleType: "none"}}>Your stored Emojis: {storeOfEmojis}</ul>
-    <p style={{fontSize: 21}}>Current Emoji: {emojiFromCurrentRender}</p>
-    <button onClick={() => queuedEmojiForRender("🤗")}>🤗</button>
-    <button onClick={() => queuedEmojiForRender("😔")}>😔</button>
-    <button onClick={() => queuedEmojiForRender("😴")}>😴</button>
-    <button onClick={() => queuedEmojiForRender("🤣")}>🤣</button>
-    <button onClick={() => queuedEmojiForRender("😭")}>😭</button>
-    <button onClick={() => queuedEmojiForRender("😇")}>😇</button>
-    <button onClick={() => queuedEmojiForRender("🤯")}>🤯</button>
-    <hr />
-    <button onClick={setStorage}>Store Emoji</button>
-    <button onClick={clearStorage}>Reset Storage</button>
-  </>
-  )
+      <h1>Emoji picker</h1>
+      <ul style={{ listStyleType: "none" }}>
+        Your stored Emojis: {storeOfEmojis}
+      </ul>
+      <p style={{ fontSize: 21 }}>Current Emoji: {state.slice(0, 1)}</p>
+      <button onClick={() => setStorage("🤗")}>🤗</button>
+      <button onClick={() => setStorage("😔")}>😔</button>
+      <button onClick={() => setStorage("😴")}>😴</button>
+      <button onClick={() => setStorage("🤣")}>🤣</button>
+      <button onClick={() => setStorage("😭")}>😭</button>
+      <button onClick={() => setStorage("😇")}>😇</button>
+      <button onClick={() => setStorage("🤯")}>🤯</button>
+      <hr />
+      <button onClick={clearStorage}>Reset Storage</button>
+    </>
+  );
 }
 
-
-function emojiStorageList (emoji:string):JSX.Element {
-  return(
-    <li style={{fontSize: 20, display:"inline-block"}}>{emoji}</li>
-  )
+function emojiStorageList(emoji: string): JSX.Element {
+  return <li style={{ fontSize: 20, display: "inline-block" }}>{emoji}</li>;
 }
-
-
-
 
 export default EmojiPicker;
-
 
 /*
 😔
 🤗
     <div>
       <h1>Which Emoji Will You Pick?</h1>
-      <button onClick={()=>queuedEmojiForRender(happy);queuedEmojiStore(emojiFromCurrentRender)}>🤗</button>
-      <button onClick={()=>queuedEmojiForRender(happy);queuedEmojiStore(emojiFromCurrentRender),nextSecondStoredEmoji(currentStoredEmoji)}>😔</button>
+      <button onClick={()=>setState(happy);setState(emojiFromCurrentRender)}>🤗</button>
+      <button onClick={()=>setState(happy);setState(emojiFromCurrentRender),nextSecondStoredEmoji(currentStoredEmoji)}>😔</button>
     </div>
 */
